709e0d4f868d66a88219b3f5501e4f1d6d48f25b
flashlack <flashlack1314@163.com>
2025-12-09 15:46:41 +0800

    chore(docs): 为SmartScheduleCoreApplication添加作者注释
    - 在类定义上方添加了作者信息注释块
    - 注释内容包含@author flash标记
    - 符合JavaDoc文档注释规范

--- src/main/java/io/github/flashlack1314/smartschedulecore/SmartScheduleCoreApplication.java
+++ src/main/java/io/github/flashlack1314/smartschedulecore/SmartScheduleCoreApplication.java
@@ -3,6 +3,10 @@ package io.github.flashlack1314.smartschedulecore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author flash
+ */
 @SpringBootApplication
 public class SmartScheduleCoreApplication {
 
