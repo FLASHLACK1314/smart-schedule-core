76c573d381c40b74968ce8c9cd87e014e6c23713
flashlack <flashlack1314@163.com>
2025-12-09 16:30:45 +0800

    feat(config): 配置文件格式从 properties 切换为 yaml
    - 添加 application-dev.yaml 到 .gitignore 忽略列表
    - 移除 application.properties 文件
    - 新增 application.yaml 并设置默认激活 dev 环境配置
    - pom.xml 中新增 spring-boot-starter-data-jpa 和 mybatis-plus 依赖
    - 在启动类中添加 @MapperScan 注解以支持 MyBatis Mapper 扫描

--- src/main/java/io/github/flashlack1314/smartschedulecore/SmartScheduleCoreApplication.java
+++ src/main/java/io/github/flashlack1314/smartschedulecore/SmartScheduleCoreApplication.java
@@ -1,5 +1,6 @@
 package io.github.flashlack1314.smartschedulecore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author flash
  */
 @SpringBootApplication
+@MapperScan("io.github.flashlack1314.smartschedulecore.mappers")
 public class SmartScheduleCoreApplication {
 
 	public static void main(String[] args) {
